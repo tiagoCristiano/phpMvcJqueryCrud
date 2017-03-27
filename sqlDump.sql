ffc8451c0e717c2d55eeff88c794161b825f49a3
tiago alves <tiago alves>
2017-03-26 21:05:56 -0300

    Alteracao na estrutura do banco

--- sqlDump.sql
+++ sqlDump.sql
@@ -13,25 +13,36 @@ MySQL - 5.6.29 : Database - tiago_teste
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`tiago_teste` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`madeira_madeira;` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 
-USE `tiago_teste`;
+USE `madeira_madeira`;
 
 /*Table structure for table `contatos` */
 
+DROP TABLE IF EXISTS `usuarios`;
 DROP TABLE IF EXISTS `contatos`;
+
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `senha` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `contatos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
   `sobre_nome` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `endereco` text COLLATE utf8_unicode_ci,
+  `email` text COLLATE utf8_unicode_ci,
+  `telefone` varchar(20) COLLATE utf8_unicode_ci,
+  `usuario_id` int(11)  NOT NULL,
+  FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `contatos` */
-
-insert  into `contatos`(`id`,`nome`,`sobre_nome`,`endereco`) values (1,'Contato','Teste','Rua:teste');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
